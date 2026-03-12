193547a53dc12f1b4cb80407301fcb425ac164c8
leandro oliveira cervantes <leandro_10_cervantes@hotmail.com>
2026-03-12 00:03:00 -0300

    fix: set base path for BrowserRouter and Vite configuration

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/web">
             <Routes>
               <Route
                 path="/"
